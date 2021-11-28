a15ced2b47a75e0c0a1b76e8e84cf29043aa6d7d
edlcorrea <playered.lc@gmail.com>
2021-11-27 23:05:49 -0300

    Add progress feedback and adjust buttons layout.

--- packages/web-client/src/components/Form/FormikStepper.jsx
+++ packages/web-client/src/components/Form/FormikStepper.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Formik, Form } from 'formik'
-import { Button } from '@material-ui/core';
+import { Button, Grid, CircularProgress } from '@material-ui/core';
 import formConfig from './formikConfig';
 import { Box } from '@material-ui/system';
 import { Stepper, Step, StepLabel } from '@material-ui/core';
@@ -10,6 +10,9 @@ export function FormikStepper({ children, ...props }) {
   const childrenArray = React.Children.toArray(children)
   const [step, setStep] = useState(0);
   const currentChild = childrenArray[step];
+  const [isCompleted, setIsCompleted] = useState(false);
+
+  const sleep = (time) => new Promise((acc) => setTimeout(acc, time));
 
   function isLastStep() {
     return (step === childrenArray.length -1);
@@ -21,26 +24,39 @@ export function FormikStepper({ children, ...props }) {
       validationSchema={ formConfig.stepValidation[step] }
       onSubmit={ async (values, helpers) => {
         if(isLastStep()) {
+          await sleep(5000);
+          console.log(values);
+          setIsCompleted(true);
           // send values to back-end!
         } else {
           setStep( s => s+1 );
         }
       }}
     >
-      <Form autoComplete='off'>
-        <Stepper sx={{mb: 8, mt: 3}} activeStep={step} alternativeLabel>
-          {childrenArray.map((child) => (
-            <Step key={child.props.label}>
-              <StepLabel> {child.props.label} </StepLabel>
-            </Step>
-          ))}
-        </Stepper>
-        {currentChild}
-        <Box sx={{mt: 5}}>
-          { step > 0 ? <Button onClick={ () => setStep( s => s-1 ) }>Anterior</Button> : null }
-          <Button type='submit'> { isLastStep() ? 'Fazae' : 'Próximo' } </Button>
-        </Box>
-      </Form>
+      {({isSubmitting}) => (
+
+        <Form autoComplete='off'>
+          <Stepper sx={{mb: 8, mt: 3}} activeStep={step} alternativeLabel>
+            {childrenArray.map((child, index) => (
+              <Step key={child.props.label} completed={ step > index || isCompleted }>
+                <StepLabel> {child.props.label} </StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          {currentChild}
+          <Grid container sx={{mt: 5}}>
+            <Grid item xs={6} textAlign='left'>
+              { step > 0 ? <Button variant='outlined' size='small'  onClick={ () => setStep( s => s-1 ) }>Anterior</Button> : null }
+            </Grid>
+            <Grid item xs={6} textAlign='right'>
+              <Button startIcon={isSubmitting ? <CircularProgress size='1rem' /> : null} disabled={isSubmitting} variant='outlined' size='small' type='submit'>
+                { isSubmitting ? 'Fazendo!' : isLastStep() ? 'Faz ae!' : 'Próximo' }
+              </Button>
+            </Grid>
+          </Grid>
+        </Form>
+
+      )}
     </Formik>
   );
 };
