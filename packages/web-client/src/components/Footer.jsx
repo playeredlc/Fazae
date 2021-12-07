f51fc0cb1b2030022819f3b10b58f3c77ca2d6b3
edlcorrea <playered.lc@gmail.com>
2021-12-07 18:39:53 -0300

    Fix: missing color in light theme.

--- packages/web-client/src/components/Footer.jsx
+++ packages/web-client/src/components/Footer.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 function Footer() {
   const devSymbol = '</>';
   return (
-    <div className='footer'><a href='https://github.com/playeredlc'><i>{devSymbol}</i> edlc</a>, 2021.</div>
+    // <div className='footer'><a href='https://github.com/playeredlc' target="_blank"><i>{devSymbol}</i> edlc</a>, 2021.</div>
+    <div className='footer'><i>{devSymbol}</i> edlc, 2021.</div>
   );
 };
 
