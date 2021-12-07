2c5ac7aa2042fc7c8e0b240306a0d64ddc6104fb
edlcorrea <playered.lc@gmail.com>
2021-12-06 21:35:09 -0300

    Add initial logic to implement Dark Mode.

--- packages/web-client/src/components/App.jsx
+++ packages/web-client/src/components/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useMemo, useState, createContext} from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import FormsHolder from './Form/FormsHolder';
 import Estimation from './Estimation/Estimation';
@@ -8,16 +9,45 @@ import Error from './Error';
 import '../../public/styles/styles.css'
 
 function App() {
+  
+  const ColorModeContext = createContext({ toggleColorMode: () => {} });
+  
+  const [mode, setMode] = useState('dark');
+  function ToggleColorMode() {
+    const colorMode = useMemo(
+      () => ({
+        toggleColorMode: () => {
+          setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+        },
+      }),
+      [],
+      );
+    };
+    
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  );
+
+  
+
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route exact path='/' element={<FormsHolder />} />
-        <Route exact path='/resultados' element={<Estimation />} />
-        <Route exact path='/error' element={<Error />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path='/' element={<FormsHolder />} />
+          <Route exact path='/resultados' element={<Estimation />} />
+          <Route exact path='/error' element={<Error />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </ThemeProvider>
   );
 }
 
